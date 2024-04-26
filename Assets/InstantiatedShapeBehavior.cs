07ef8f3b70073b675e5459d195c1257eeca8d410
ak2411 <angelicakosasih24@gmail.com>
2024-04-25 21:03:40 -0700

    chore: wip add platforms

---
+++ Assets/InstantiatedShapeBehavior.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatedShapeBehavior : MonoBehaviour
+{
+    // Handles detection whenever it collides with a gameobject to see if it is a platform. if it is stil within the boundaries of the platform once the user stops selecting, update its parent to the patform parent
+}
--- Assets/PlatformBehavior.cs
+++ Assets/PlatformBehavior.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 using Oculus.Interaction;
 using Oculus.Interaction.Input;
 
+public enum PlatformType
+{
+    Red, Green, Blue, Orange
+};
+
 public class PlatformBehavior : MonoBehaviour
 {
-    // TODO: Grabbable with move and rotate
-    // TODO: Instantiate at a position in the center
-    
+    private string owner;
+    [SerializeField]
+    private PlatformType platformId;
+    private GameObject ownerSymbol;
+    protected void Awake()
+    {
+        // Using the platformId, update the appropriate material
+        // Update button colors
+        
+    }
+
+    private void connectWithLocalUser(string requestUser)
+    {
+        if (owner != null) return;
+        owner = requestUser;
+        // make outline the appropriate color
+        // update user's hands
+    }
+
+    private void disconnectFromOwner()
+    {
+        owner = null;
+    }
 }
